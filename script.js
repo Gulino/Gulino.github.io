6ef17e82b0942b37e210f88ed128f38c209a31ba
Martina Gulino <martina.gulino01@icatt.it>
2018-05-01 20:38:59 -0400

    lh

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+  $("button").click(function(){
+          $("#miami2").fadeIn("slow");
+
+});
+
+$("#hide").click(function(){
+    $("#p").hide();
+});
+$("#show").click(function(){
+    $("#p").show();
+});
+
+
 });
